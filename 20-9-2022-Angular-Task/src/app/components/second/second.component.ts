8198edb6102e70a36545832f19943492d86e569b
cathyjoanne <cathyjoanne14@gmail.com>
2022-10-03 00:18:09 -0700

    added ng on second page

--- 20-9-2022-Angular-Task/src/app/components/second/second.component.ts
+++ 20-9-2022-Angular-Task/src/app/components/second/second.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./second.component.css']
 })
 export class SecondComponent implements OnInit {
-
+  color:string='white';
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  isblue(){
+    this.color = 'lightblue';
+  }
+  ispink(){
+    this.color = 'lightpink';
+  }
+  isgreen(){
+    this.color = 'lightgreen';
+  }
 }
